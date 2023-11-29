385701eeba74473e4bc60f4d953bca18d02a7cf6
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-11-28 17:05:00 -0800

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-print(''.join(["{:c}".format(122 - i)
-    if i % 2 == 0
-    else "{:c}".format(90 - i // 2)
-    for i in range(26)]))
+print(''.join(["{:c}".format(122 - i // 2) if i % 2 == 0 else "{:c}".format(90 - i // 2) 
+               for i in range(26)]))
