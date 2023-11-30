42931120b67390ccf045e693bcfc6e99e8aad071
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-11-30 12:53:22 -0800

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     count = 0
     if num == 0:
         print("0 arguments.")
+    elif num == 1:
+        print("1 arguments:")
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(num))
         for arg in sys.argv:
