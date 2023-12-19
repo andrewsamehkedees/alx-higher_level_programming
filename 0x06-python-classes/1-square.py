6240951ff9cd684764dfc144eb893057abd95ebe
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-12-19 07:33:12 -0800

    test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,4 +4,9 @@ class Square:
     It is used to control the size attribute in a square object.
     """
     def __init__(self, size):
+        """Creates new instances of square (1 side).
+
+        Args:
+            size: size of the square.
+        """
         self.__size = size
