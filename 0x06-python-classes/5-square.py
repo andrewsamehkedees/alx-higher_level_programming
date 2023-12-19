8a56a4ad60156919b60f88a16eab55c9acfc5af7
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-12-19 08:20:00 -0800

    test

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""define class square"""
+
+class Square:
+    """This is a class that defines a square.
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """this is the constractor
+        Args:
+            size: the default valeo is 0
+        Raises:
+            TypeError: the type of the input
+            ValueError: the value of the input
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """class area
+        Return: the square of the size
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
