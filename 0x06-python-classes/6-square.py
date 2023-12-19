663d924ac8a062fd6c689957d9ce9057e0633ed7
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-12-19 08:24:34 -0800

    test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Defines a class Square"""
+
+
 class Square:
     """This is a class that defines a square.
     """
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -20,6 +23,7 @@ class Square:
             TypeError: the type of the input
             ValueError: the value of the input
         """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -39,6 +43,7 @@ class Square:
         Raises:
             TypeError: the type of the input
         """
+
         if not isinstance(value, tuple) or len(value) != 2 or \
            not all(isinstance(n, int) for n in value) or \
            not all(n >= 0 for n in value):
