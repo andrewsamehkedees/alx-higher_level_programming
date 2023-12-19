780c0d5786c263d967e8e19be4a1e84a301de3e7
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-12-19 08:03:17 -0800

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,11 +2,13 @@
 class Square:
     """This is a class that defines a square.
     """
+
     def __init__(self, size=0):
-    """this is the constractor
-    Args:
-        size: the default valeo is 0
-    """
+        """this is the constractor
+        Args:
+            size: the default valeo is 0
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
