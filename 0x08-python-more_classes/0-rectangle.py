0b31cc5cee7bd368ea7f1a909642a35d28033d7f
andrewsamehkedees <andrewsamehkedees@gmail.com>
2024-01-05 12:40:07 -0800

    test

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This is a module that defines a Rectangle class and related methods.
+"""
 class Rectangle:
+    """
+    This is a class that defines a Rectangle.
+    """
     pass
