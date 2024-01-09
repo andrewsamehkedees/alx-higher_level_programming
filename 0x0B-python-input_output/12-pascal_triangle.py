7915883bd7b8ddbf938b79f9cae31b22c0f0132e
andrewsamehkedees <andrewsamehkedees@gmail.com>
2024-01-09 14:45:21 -0800

    test

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal triangle module"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
