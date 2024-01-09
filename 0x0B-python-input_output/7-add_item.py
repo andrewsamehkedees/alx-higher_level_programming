b447291c1fca384d8a875a2518b45ea2b127d55d
andrewsamehkedees <andrewsamehkedees@gmail.com>
2024-01-09 14:26:27 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-# Import the required functions
+""" Import the required functions"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# Try to load the list from the file, or create a new list if the file doesn't exist
+""" Try to load the list from the file, or create a new list if the file doesn't exist"""
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
 
-# Add all command line arguments to the list
+""" Add all command line arguments to the list"""
 my_list.extend(sys.argv[1:])
 
-# Save the list back to the file
+"""Save the list back to the file"""
 save_to_json_file(my_list, filename)
